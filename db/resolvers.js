753bfbd51215c941a320bff6c4acfbfa779ff0d7
Diana Rut <93566502+DIANARUTGARCIA@users.noreply.github.com>
2023-05-10 15:44:48 -0600

    Conectando a BD

---
+++ db/resolvers.js
@@ -0,0 +1,29 @@
+const { ApolloServer, gql } = require('apollo-server')
+
+const cursos = [
+  {
+    titulo: 'JavaScript Moderno Guia Definiva construye +10 proyectos',
+    tecnologia: 'Javascript ES6',
+  },
+  {
+    titulo: 'React - a guia completa Hooks context redux Mern +15 apps',
+    tecnologia: 'React',
+  },
+  {
+    titulo: 'Node.js - Bootcamp desarrolo web inc RVC y Rest API´s',
+    tecnologia: 'Node.js',
+  },
+  {
+    titulo: 'React Js Avanzado - Fullstack React GraphQL y apollo',
+    tecnologia: 'React',
+  },
+]
+// Crear los resolvers
+const resolvers = {
+  Query: {
+    obtenerCursos: () => cursos,
+    obtenerTecnologia: () => cursos,
+  },
+}
+
+module.exports = resolvers;
--- index.js
+++ index.js
@@ -1,42 +1,11 @@
 const { ApolloServer, gql } = require('apollo-server')
+const typeDefs = require('./db/schema')
+const resolvers = require('./db/resolvers')
 
-const typeDefs = gql`
-  type Curso {
-    titulo: String
-  }
+const conectarDB = require('./config/db')
 
-  type Query {
-    obtenerCursos: [Curso]
-  }
-`
-
-// Dummy data to conect the server
-const cursos = [
-  {
-    titulo: 'JavaScript Moderno Guia Definiva construye +10 proyectos',
-    tecnologia: 'Javascript ES6',
-  },
-  {
-    titulo: 'React - a guia completa Hooks context redux Mern +15 apps',
-    tecnologia: 'React',
-  },
-  {
-    titulo: 'Node.js - Bootcamp desarrolo web inc RVC y Rest API´s',
-    tecnologia: 'Node.js',
-  },
-  {
-    titulo: 'React Js Avanzado - Fullstack React GraphQL y apollo',
-    tecnologia: 'React',
-  },
-]
-
-// Crear los resolvers
-const resolvers = {
-  Query: {
-    obtenerCursos: () => cursos,
-  },
-}
-
+//conectar a BD
+conectarDB();
 const server = new ApolloServer({ typeDefs, resolvers })
 
 server.listen().then(({ url }) => {
